1453e0906a19bc115929cafd638c84509ad56264
Miguel <blank0010b@gmail.com>
2025-02-09 18:36:08 +0000

    1.1

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import time
+import logging
 from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -14,6 +15,23 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 import xml.etree.ElementTree as ET
 import argparse
+
+# Create a logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# Create a file handler and a stream handler
+file_handler = logging.FileHandler('web_monitor.log')
+stream_handler = logging.StreamHandler()
+
+# Create a formatter and add it to the handlers
+formatter = logging.Formatter('(%(asctime)s) [%(levelname)s] %(message)s')
+file_handler.setFormatter(formatter)
+stream_handler.setFormatter(formatter)
+
+# Add the handlers to the logger
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
 
 def load_params(file_path):
     """
@@ -70,8 +88,10 @@ def monitor_website(browser):
         driver.maximize_window()
 
         # [1] Access website
+        logger.info(f'Accessing URL: \'{params["website_url"]}\'')
         driver.get(params["website_url"])
         driver_wait = WebDriverWait(driver, params["event_wait"])
+        logger.info(f'Successfully accessed \'{params["website_url"]}\'')
 
         # [2] Handle consent popup
         driver_wait.until(
@@ -82,6 +102,7 @@ def monitor_website(browser):
                 ),
             ),
         ).click()
+        logger.info("Cookies popup clicked.")
 
         # [3] Scroll down and click "Find an agency"
         driver_wait.until(
@@ -92,11 +113,15 @@ def monitor_website(browser):
                     ),
             ),
         ).click()
+        logger.info("Find agency button clicked.")
 
         # [4] Fill the form and search
 
         # Fill in the first text box
-        driver.find_element(By.ID, "em-search-form__searchstreet").send_keys(params["agency_name"])
+        driver.find_element(
+            By.ID,
+            "em-search-form__searchstreet",
+        ).send_keys(params["agency_name"])
 
         # Wait until the first text box contains the expected value
         WebDriverWait(driver, 10).until(
@@ -105,9 +130,13 @@ def monitor_website(browser):
                 "em-search-form__searchstreet",
             ).get_attribute("value") == params["agency_name"]
         )
+        logger.info(f"Searching agency \'{params['agency_name']}\'...")
 
         # Fill in the second text box
-        driver.find_element(By.ID, "em-search-form__searchcity").send_keys(params["agency_postal_code"])
+        driver.find_element(
+            By.ID,
+            "em-search-form__searchcity"
+        ).send_keys(params["agency_postal_code"])
 
         # Wait until the second text box contains the expected value
         WebDriverWait(driver, 10).until(
@@ -116,6 +145,7 @@ def monitor_website(browser):
                 "em-search-form__searchcity",
             ).get_attribute("value") == params["agency_postal_code"]
         )
+        logger.info(f"Searching agency postal code \'{params['agency_postal_code']}\'...")
 
         # Wait for the search button to be clickable and click it
         driver_wait.until(
@@ -136,15 +166,15 @@ def monitor_website(browser):
                 ),
             ),
         )
-        # TODO: Buffer here
         ActionChains(driver).double_click(agency_waypoint).perform()
+        logger.info(f"Agency \'{params["agency_name"]}\' @ {params['agency_postal_code']} found.")
         
         time.sleep(2)
 
-        print("Success")
+        logger.info(f"\'{params["website_url"]}\' is up and running.")
 
     except Exception as e:
-        print(f"[ERROR] {e}")
+        logger.error(f"{e}")
         log_path = os.path.join("logs", LOG_DATE)
         os.makedirs(log_path, exist_ok=True)
         screenshot_path = os.path.join(log_path, f"error_screenshot_{browser}.png")
